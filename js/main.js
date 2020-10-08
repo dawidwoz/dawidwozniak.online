f83c574b73054405be6b7af229a0f6e85ff3d7ce
Dawid Wozniak <daw79@aber.ac.uk>
2020-10-08 11:52:56 +0100

    feat: Add Ajax call to json file

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-import json from '../data/data.json';
-json.forEach(x => { console.log(x.firstname, x.lastname); });
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var config = JSON.parse(this.responseText);
+        const app = Vue.createApp({
+            data() {
+                return {
+                    config
+                }
+            },
+            methods: {
+                returnImg(id) {
+                    return "./img/" + id;
+                }
+            }
+        });
+       app.mount('#app');
+    }
+};
+xmlhttp.open("GET", "data/data.json", true);
+xmlhttp.send();
+
+    
