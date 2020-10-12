b5f936fb57effe54cd4a805ab2347fc58024b780
Dawid Wozniak <daw79@aber.ac.uk>
2020-10-13 00:13:48 +0100

    feat: Add footer and add common script for all components

---
+++ js/common.js
@@ -0,0 +1,6 @@
+function convertStringToHTML(selector , element) {
+        $(document).ready(function(){ //Need to check if the element is there
+         var el = $(selector);
+         el.html(element);
+        });
+}
